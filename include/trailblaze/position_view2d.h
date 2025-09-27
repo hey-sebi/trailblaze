52d0f5be226aa47b6c03d7974979a3fa5a0ddc39
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2025-09-27 22:28:57 +0200

    Add position view classes

---
+++ include/trailblaze/position_view2d.h
@@ -0,0 +1,52 @@
+#ifndef TRAILBLAZE_POSITION_VIEW_2D_H_
+#define TRAILBLAZE_POSITION_VIEW_2D_H_
+#include <type_traits>
+
+namespace trailblaze {
+
+/// Provides a view onto a position in 2D space
+template <class T>
+class PositionView2D
+{
+ public:
+  using value_type = std::remove_const_t<T>;
+  using reference = T&;
+  using pointer = T*;
+
+  /** Constructor
+   *  @param x X-component of the position.
+   *  @param y Y-component of the position.
+   */
+  PositionView2D(T& x, T& y) : x_(&x), y_(&y)
+  {
+  }
+
+  /// Access to the X component of the position
+  reference X() const noexcept
+  {
+    return *x_;
+  }
+
+  /// Access to the Y component of the position
+  reference Y() const noexcept
+  {
+    return *y_;
+  }
+
+ private:
+  /// points to the viewed x value
+  pointer x_;
+  /// points to the viewed y value
+  pointer y_;
+};
+
+/// Factory function to create a @see PositionView2D
+template <class U>
+auto MakePositionView2D(U& x, U& y) -> PositionView2D<U>
+{
+  return PositionView2D<U>(x, y);
+}
+
+}  // namespace trailblaze
+
+#endif
---
+++ include/trailblaze/position_view3d.h
@@ -0,0 +1,61 @@
+#ifndef TRAILBLAZE_POSITION_VIEW_3D_H_
+#define TRAILBLAZE_POSITION_VIEW_3D_H_
+#include <type_traits>
+
+namespace trailblaze {
+
+/// Provides a view onto a position in 3D space
+template <class T>
+class PositionView3D
+{
+ public:
+  using value_type = std::remove_const_t<T>;
+  using reference = T&;
+  using pointer = T*;
+
+  /** Constructor
+   *  @param x X-component of the position.
+   *  @param y Y-component of the position.
+   *  @param z Z-component of the position.
+   */
+  PositionView2D(T& x, T& y, T& z) : x_(&x), y_(&y), z_(&z)
+  {
+  }
+
+  /// Access to the X-component of the position
+  reference X() const noexcept
+  {
+    return *x_;
+  }
+
+  /// Access to the Y-component of the position
+  reference Y() const noexcept
+  {
+    return *y_;
+  }
+
+  /// Access to the Z-component of the position
+  reference Z() const noexcept
+  {
+    return *z_;
+  }
+
+ private:
+  /// points to the viewed x value
+  pointer x_;
+  /// points to the viewed y value
+  pointer y_;
+  /// points to the viewed z value
+  pointer z_;
+};
+
+/// Factory function to create a @see PositionView3D
+template <class U>
+auto MakePositionView3D(U& x, U& y) -> PositionView3D<U>
+{
+  return PositionView3D<U>(x, y);
+}
+
+}  // namespace trailblaze
+
+#endif
